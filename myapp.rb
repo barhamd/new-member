0f862caafce6a595f7dd7e5202a9cd457fb02b4d
Devin Barham <barhamd@gmail.com>
2013-10-14 02:05:44 -0400

    Added basic email configuration. Not quite functioning

--- myapp.rb
+++ myapp.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
 require 'haml'
+require 'pony'
 
 get '/' do
   haml :index
+end
+
+post '/submit' do
+  Pony.mail :to => 'barhamd@gmail.com', :via => :smtp, :smtp => {
+            :host => 'smtp.gmail.com',
+            :port => 465,
+            :user_name => '',
+            :password => '',
+            :authentication => 'plain'
+            },
+            :subject => 'It works!'
 end

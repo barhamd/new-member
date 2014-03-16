621af2c009eeb8e74fde7e3520b0172d31a84839
Devin Barham <barhamd@gmail.com>
2014-03-16 15:36:52 -0400

    Post to action. Configure emails with ENV vars.

--- myapp.rb
+++ myapp.rb
@@ -7,12 +7,19 @@ get '/' do
 end
 
 post '/submit' do
-  Pony.mail :to => 'barhamd@gmail.com', :via => :smtp, :smtp => {
-            :host => 'smtp.gmail.com',
-            :port => 465,
-            :user_name => '',
-            :password => '',
-            :authentication => 'plain'
-            },
-            :subject => 'It works!'
+  Pony.mail({
+    :to => ENV['TO_EMAIL'],
+    :via => :smtp,
+    :via_options => {
+      :address              => 'smtp.gmail.com',
+      :port                 => '587',
+      :enable_starttls_auto => true,
+      :user_name            => ENV['FROM_EMAIL'],
+      :password             => ENV['FROM_EMAIL_PASSWORD'],
+      :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+      :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+    }
+  })
+
+  haml :submit
 end

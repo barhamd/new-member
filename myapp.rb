4edbd5d40829c3789361699f78630ecb9dec92b6
Devin Barham <barhamd@gmail.com>
2014-03-16 16:03:56 -0400

    Submit sends email with all info

--- myapp.rb
+++ myapp.rb
@@ -9,6 +9,21 @@ end
 post '/submit' do
   Pony.mail({
     :to => ENV['TO_EMAIL'],
+    :subject => 'Potential Member Info!',
+    :html_body => "
+      <h1> Here's the info!</h1>
+      <ul>
+        <li>First Name: #{params[:first_name]}</li>
+        <li>Last Name: #{params[:last_name]}</li>
+        <li>Phone: #{params[:phone]}</li>
+        <li>Email: #{params[:email]}</li>
+        <li>Address: #{params[:address]}</li>
+        <li>Emergency Contact: #{params[:emergency_contact]}</li>
+        <li>Emergency Contact Phone: #{params[:emergency_contact_phone]}</li>
+        <li>Relationship: #{params[:relationship]}</li>
+        <li>Preferred ID: #{params[:preferred_id]}</li>
+      </ul>
+    ",
     :via => :smtp,
     :via_options => {
       :address              => 'smtp.gmail.com',
